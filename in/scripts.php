263be3adabc28c2ef06830a29f4e46bb1bddb986
airton <airtonvancin@gmail.com>
2013-09-12 12:34:45 -0300

    Atualizacao caminho script

--- in/scripts.php
+++ in/scripts.php
@@ -1,7 +1,7 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/libs/jquery-1.9.0.min.js"><\/script>')</script>
 
-<script src="js/scripts.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
 
 <!-- Google Analytics -->
 <script>
