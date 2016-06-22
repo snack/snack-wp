16ce67ba23e45e969bd4bcb5129158036921d8dc
Guilherme Lima <guilherme.lima@a2ad.com.br>
2016-06-22 15:50:51 -0300

    Remove description and keywords meta, add utf-8 and X-UA-Compatible. Modify google analytic script

--- header.php
+++ header.php
@@ -9,15 +9,15 @@
     <title><?php wp_title(""); ?></title>
 
     <!-- Metas -->
-    <meta name="description" content="">
-    <meta name="keywords" content="">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" href="favicon.ico">
-    <link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
 
     <?php wp_head(); ?>
 
@@ -25,17 +25,20 @@
     <!--[if lt IE 9]>
         <script src="<?php echo get_template_directory_uri(); ?>/build/js/libs/html5shiv-respond.min.js"></script>
     <![endif]-->
-
-    <script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='https://www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-XXXXX-X','auto');
-    ga('require', 'displayfeatures');
-    ga('send', 'pageview');
-    </script>
+    <?php
+        $ga_code = get_option("ga_ua_code");
+        if ( $ga_code ): ?>
+        <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','<?php echo $ga_code;?>','auto');
+        ga('require', 'displayfeatures');
+        ga('send', 'pageview');
+        </script>
+    <?php endif; ?>
 
 </head>
 <body <?php body_class(); ?>>
