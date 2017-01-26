f1b8fe13b2a82e7eb8c57f410bddc5d925268b2d
Guilherme Lima <guilherme.lima@a2ad.com.br>
2017-01-25 23:21:22 -0200

    Adjust a class

--- inc/atoms/snack-social-share-buttons.php
+++ inc/atoms/snack-social-share-buttons.php
@@ -8,13 +8,13 @@
  *
  * @return string         Return the social share buttons.
  */
-function snack_social_share($sharetitle = '', $facebook = true, $twitter = true, $googleplus = true, $linkedin = true, $small = false){
+function snack_social_share($sharetitle = '', $facebook = true, $twitter = true, $googleplus = true, $linkedin = true, $small = true){
 
     $permalink = get_permalink();
     $title     = get_the_title();
 
     if($small == true ){
-        $classe = ' social-small';
+        $classe = 'btn-small';
     }else{
         $classe = '';
     }
@@ -23,22 +23,22 @@ function snack_social_share($sharetitle = '', $facebook = true, $twitter = true,
     $html .= ( $sharetitle ) ? '<h3 class="compartilhar">'.$sharetitle.'</h3>' : '';
 
     if ($facebook == true) {
-        $html .= '<a href="'.$permalink.'" class="social-button social-facebook" alt="Compartilhar via Facebook" data-social="facebook" data-title="'.$title.'">';
+        $html .= '<a href="'.$permalink.'" class="social-btn social-facebook" alt="Compartilhar via Facebook" data-social="facebook" data-title="'.$title.'">';
         $html .= '<i class="fa fa-facebook"></i>Facebook';
         $html .= '</a>';
     }
     if ($twitter == true) {
-        $html .= '<a href="'.$permalink.'" class="social-button social-twitter" alt="Compartilhar via Twitter" data-social="twitter" data-title="'.$title.'" data-hashtags="">';
+        $html .= '<a href="'.$permalink.'" class="social-btn social-twitter" alt="Compartilhar via Twitter" data-social="twitter" data-title="'.$title.'" data-hashtags="">';
         $html .= '<i class="fa fa-twitter"></i>Twitter';
         $html .= '</a>';
     }
     if ($googleplus == true) {
-        $html .= '<a href="'.$permalink.'" class="social-button social-google-plus" alt="Compartilhar via Google+" data-social="googleplus" data-title="'.$title.'">';
+        $html .= '<a href="'.$permalink.'" class="social-btn social-google-plus" alt="Compartilhar via Google+" data-social="googleplus" data-title="'.$title.'">';
         $html .= '<i class="fa fa-google-plus"></i>Google+';
         $html .= '</a>';
     }
     if ($linkedin == true) {
-        $html .= '<a href="'.$permalink.'" class="social-button social-linkedin" alt="Compartilhar via Linkedin" data-social="linkedin" data-title="'.$title.'">';
+        $html .= '<a href="'.$permalink.'" class="social-btn social-linkedin" alt="Compartilhar via Linkedin" data-social="linkedin" data-title="'.$title.'">';
         $html .= '<i class="fa fa-linkedin"></i>Linkedin';
         $html .= '</a>';
     }
