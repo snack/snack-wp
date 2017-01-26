ec830e01561c47e8ef18fcc3cd96da407821f9c3
Guilherme Lima <guilherme.lima@a2ad.com.br>
2017-01-25 23:49:36 -0200

    Adjust 404 template

--- 404.php
+++ 404.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
-<main id="container conteudo" tabindex="-1" role="main">
-    <h1 class="h1 titulo-big">Página não encontrada</h1>
-    <p>Desculpe-nos, mas a página solicitada não foi encontrada ou pode ter sido removida. Por favor, volte à página inicial para continuar a navegação.</p>
-    <p><a href="<?php echo site_url('/'); ?>"><i class="fa fa-arrow-circle-o-left"></i> Voltar</a></p>
+<main id="conteudo" tabindex="-1" role="main">
+	<div class="container">
+	    <h1 class="h1 titulo-big">Página não encontrada</h1>
+	    <p>Desculpe-nos, mas a página solicitada não foi encontrada ou pode ter sido removida. Por favor, volte à página inicial para continuar a navegação.</p>
+	    <p><a href="<?php echo site_url('/'); ?>"><i class="fa fa-arrow-circle-o-left"></i> Voltar</a></p>
+    </div>
 </div>
+<?php get_footer(); ?>
